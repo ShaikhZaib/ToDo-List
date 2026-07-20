0d02e895378afd02aa9e1e7afdfc41b29b72b48e
Shaikh Zaib <shaikhzaib04@gmail.com>
2026-07-20 23:49:40 +0530

    Add functionality to add new projects and select the choosen project

--- src/ui/displayController.js
+++ src/ui/displayController.js
@@ -1,11 +1,14 @@
 import initialLoad from "./initialLoad.js";
 import ProjectsManager from "../models/ProjectsManager.js";
+import Project from "../models/Project.js";
 
 const DisplayController = (() => {
   const ui = initialLoad();
   const projectManager = new ProjectsManager();
 
-  let currentProject = projectManager.getProject(projectManager.projects[0].id);
+  let currentProject = projectManager.getProject(
+    projectManager.getProjects()[0].id,
+  );
 
   function renderProjects() {
     ui.projectsList.innerHTML = "";
@@ -17,17 +20,63 @@ const DisplayController = (() => {
       projectButton.textContent = project.name;
       projectButton.dataset.id = project.id;
 
-      projectLi.appendChild(projectButton);
+      if (project.id === currentProject.id) {
+        projectButton.classList.add("active");
+      }
 
+      projectLi.appendChild(projectButton);
       ui.projectsList.append(projectLi);
     });
   }
 
-  function renderTodos() {}
+  function renderTodos() {
+    ui.todoHeading.textContent = currentProject.name;
+
+    ui.todosList.innerHTML = "";
+
+    currentProject.todos.forEach();
+  }
+
+  function handleAddProject() {
+    const projectName = prompt("Enter your project name: ");
+
+    if (projectName === null) {
+      return;
+    }
+
+    const trimmedName = projectName.trim();
+
+    if (trimmedName === "") {
+      return;
+    }
+
+    const project = new Project({
+      name: trimmedName,
+    });
+
+    projectManager.addProject(project);
+
+    renderProjects();
+  }
+
+  function handleProjectSelection(event) {
+    const button = event.target.closest("button");
+    if (!button) return;
+
+    const projectId = button.dataset.id;
+
+    currentProject = projectManager.getProject(projectId);
+
+    renderProjects();
+    renderTodos();
+  }
 
   function init() {
     renderProjects();
     renderTodos();
+
+    ui.addProjectButton.addEventListener("click", handleAddProject);
+    ui.projectsList.addEventListener("click", handleProjectSelection);
   }
 
   return {
